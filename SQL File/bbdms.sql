5ebf973b8b8cb0d6547ced5e2f37415d18465dd3
tnshaila <1920926@iub.edu.bd>
2023-10-09 01:42:33 +0600

    Modified Database

--- SQL File/bbdms.sql
+++ SQL File/bbdms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 27, 2022 at 06:34 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: Oct 08, 2023 at 09:39 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,14 +35,14 @@ CREATE TABLE `tbladmin` (
   `Email` varchar(200) DEFAULT NULL,
   `Password` varchar(200) DEFAULT NULL,
   `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tbladmin`
 --
 
 INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
-(1, 'Admin', 'admin', 8979555558, 'admin@gmail.com', '202cb962ac59075b964b07152d234b70', '2022-07-28 04:36:52');
+(1, 'Admin', 'admin', 8979555558, 'admin@gmail.com', 'admin123', '2022-07-27 22:36:52');
 
 -- --------------------------------------------------------
 
@@ -63,18 +63,19 @@ CREATE TABLE `tblblooddonars` (
   `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` int(1) DEFAULT NULL,
   `Password` varchar(250) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblblooddonars`
 --
 
 INSERT INTO `tblblooddonars` (`id`, `FullName`, `MobileNumber`, `EmailId`, `Gender`, `Age`, `BloodGroup`, `Address`, `Message`, `PostingDate`, `status`, `Password`) VALUES
-(6, 'Ashu Misra', '7797987981', 'ashu@gmail.com', 'Male', 35, 'O-', 'H-900, Ravinderpuri colony, Varanasi', ' Call me if blood require', '2022-05-17 12:43:41', 1, '202cb962ac59075b964b07152d234b70'),
-(9, 'Test Demo', '9789797979', 'test@gmail.com', 'Male', 56, 'A+', 'Allahabad', ' gjhgjhgjhghjghj', '2022-05-21 06:09:08', 1, '202cb962ac59075b964b07152d234b70'),
-(10, 'John Doe', '1236547890', 'johnd@gmail.com', 'Male', 25, 'O-', 'A 5623 XYZ Street New Delhi', ' NA', '2022-07-29 01:50:58', 1, 'f925916e2754e5e03f75dd58a5733251'),
-(11, 'Amit Kumar', '1231231230', 'amitk@gmail.com', 'Male', 26, 'AB+', 'ABc Street Sector 20 Noida UP', ' NA', '2022-08-02 01:22:52', 1, 'f925916e2754e5e03f75dd58a5733251'),
-(12, 'Anuj kumar', '1425362514', 'ak@test.com', 'Male', 30, 'A-', 'NA', ' NA', '2022-12-27 17:31:08', 1, 'f925916e2754e5e03f75dd58a5733251');
+(6, 'Toha', '0011', 'toha@gmail.com', 'Male', 35, 'AB-', 'Barishal', 'Call me if blood require', '2022-05-17 12:43:41', 1, 'hello'),
+(9, 'Joyita', '0011', 'joyita@gmail.com', 'Male', 16, 'A+', 'Dhaka', 'Call me if blood require', '2022-05-21 06:09:08', 1, 'hello'),
+(10, 'Shad', '0011', 'shad@gmail.com', 'Male', 25, 'O-', 'Rajshahi', ' NA', '2022-07-29 01:50:58', 1, 'hello'),
+(11, 'Noor', '0011', 'noor@gmail.com', 'Male', 26, 'AB+', 'Chittagong', ' NA', '2022-08-02 01:22:52', 1, 'hello'),
+(12, 'Sourav', '0011', 'sourav@gmail.com', 'Male', 30, 'A-', 'Khulna', ' NA', '2022-12-27 17:31:08', 1, 'hello'),
+(14, 'Shaila', '0011', 'shaila@gmail.com', 'Female', 0, 'O+', 'Dhaka', 'Call me if blood require', '2023-10-06 13:49:32', 1, '5d41402abc4b2a76b9719d911017c592');
 
 -- --------------------------------------------------------
 
@@ -86,7 +87,7 @@ CREATE TABLE `tblbloodgroup` (
   `id` int(11) NOT NULL,
   `BloodGroup` varchar(20) DEFAULT NULL,
   `PostingDate` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblbloodgroup`
@@ -96,7 +97,7 @@ INSERT INTO `tblbloodgroup` (`id`, `BloodGroup`, `PostingDate`) VALUES
 (1, 'A-', '2022-04-30 20:33:50'),
 (2, 'AB-', '2022-04-30 20:34:00'),
 (3, 'O-', '2022-04-30 20:34:00'),
-(4, 'A-', '2022-04-30 20:34:00'),
+(4, 'O+', '2022-04-30 20:34:00'),
 (5, 'A+', '2022-04-30 20:34:00'),
 (6, 'AB+', '2022-04-30 20:34:00');
 
@@ -115,7 +116,7 @@ CREATE TABLE `tblbloodrequirer` (
   `BloodRequirefor` varchar(250) DEFAULT NULL,
   `Message` mediumtext DEFAULT NULL,
   `ApplyDate` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblbloodrequirer`
@@ -126,7 +127,8 @@ INSERT INTO `tblbloodrequirer` (`ID`, `BloodDonarID`, `name`, `EmailId`, `Contac
 (2, 3, 'Mukesh', 'muk@gmail.com', 5896231478, 'Others', 'Please help', '2022-05-17 11:58:48'),
 (3, 6, 'Hitesh', 'hit@gmail.com', 1236547896, 'Brother', 'do the needful', '2022-05-17 12:02:12'),
 (4, 10, 'Rahul Singh', 'rahk@gmail.com', 2536251425, 'Mother', 'Please help me', '2022-07-29 01:51:52'),
-(5, 11, 'Anuj Kumar', 'ak@gmail.com', 8525232102, 'Others', 'Need blood on urgent basis', '2022-08-02 01:24:18');
+(5, 11, 'Anuj Kumar', 'ak@gmail.com', 8525232102, 'Others', 'Need blood on urgent basis', '2022-08-02 01:24:18'),
+(6, 10, 'Shaila', 'shaila@gmail.com', 11, 'Father', 'hello', '2023-10-06 13:14:14');
 
 -- --------------------------------------------------------
 
@@ -139,7 +141,7 @@ CREATE TABLE `tblcontactusinfo` (
   `Address` tinytext DEFAULT NULL,
   `EmailId` varchar(255) DEFAULT NULL,
   `ContactNo` char(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblcontactusinfo`
@@ -162,7 +164,7 @@ CREATE TABLE `tblcontactusquery` (
   `Message` longtext DEFAULT NULL,
   `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -175,7 +177,7 @@ CREATE TABLE `tblpages` (
   `PageName` varchar(255) DEFAULT NULL,
   `type` varchar(255) NOT NULL DEFAULT '',
   `detail` longtext NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblpages`
@@ -249,7 +251,7 @@ ALTER TABLE `tbladmin`
 -- AUTO_INCREMENT for table `tblblooddonars`
 --
 ALTER TABLE `tblblooddonars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tblbloodgroup`
@@ -261,7 +263,7 @@ ALTER TABLE `tblbloodgroup`
 -- AUTO_INCREMENT for table `tblbloodrequirer`
 --
 ALTER TABLE `tblbloodrequirer`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tblcontactusinfo`
